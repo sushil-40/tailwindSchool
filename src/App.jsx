b765324ca5d7313495712c7e57dbb2e50ee36da9
Sushil <sushildangoriya40@gmail.com>
2025-09-20 19:52:40 +1000

    tailwind school started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500 hover:bg-amber-300 mb-4">How are you?</h1>
+      <button className="bg-[lightseagreen] rounded-[7px] p-2 px-10">
+        Click Me
+      </button>
+    </>
+  );
+}
+
+export default App;
